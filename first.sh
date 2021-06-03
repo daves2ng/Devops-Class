cea78e3def609d4b27f8e6d2e6ee0cc547a93631
Ubuntu <ubuntu@ip-172-31-89-165.ec2.internal>
2021-06-03 21:13:44 +0000

    I just made a little mode o this to test another commit

--- first.sh
+++ first.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-ls
 echo "Welcome to GitHub"
 echo "Seems you are excited..... I am to actually"
 sleep 5
 clear
 echo "time to go. you have actually done a good job"
+echo "if this is seem, it means the whole git process works"
