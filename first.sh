3f8a9cc57200639d6795260f2b85305021ab5387
Dave <30566800+daves2ng@users.noreply.github.com>
2021-06-03 22:23:15 +0100

    Update first.sh
    this modification was done from git directly and git pulled

--- first.sh
+++ first.sh
@@ -5,3 +5,6 @@ sleep 5
 clear
 echo "time to go. you have actually done a good job"
 echo "if this is seem, it means the whole git process works"
+clear
+echo "if this can be seen in my EC2 running instance, then git put is working great and as expected"
+echo "This actually looks awesome"
